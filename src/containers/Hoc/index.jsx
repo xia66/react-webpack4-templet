5aa0318745de3cee4e97857490c2d19d85ee0ee6
xiachengyu_i <xiachengyu_i@didichuxing.com>
2018-12-03 16:29:57 +0800

    update

---
+++ src/containers/Hoc/index.jsx
@@ -0,0 +1,20 @@
+/*************************
+高阶组件测验              
+                    xia
+                2018/11/29
+**************************/
+import React from 'react'
+
+export default class HocTest extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return(
+            <div>
+                123
+            </div>)
+    }
+}
